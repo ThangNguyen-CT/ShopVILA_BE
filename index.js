878cd66fbf3f062b898b46812fe3a9ad1ceaa913
ThangNguyen-CT <thangnguyen1333@gmail.com>
2023-03-23 21:27:48 +0700

    v1

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const dbConnect = require('./config/dbConnect');
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT ||4000;
-const productRoter= require('./routers/productRoute')
+const PORT = process.env.PORT || 4000;
+const productRoter = require('./routers/productRoute')
 const authRoter = require('./routers/authRoute.js')
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 app.use(cookieParser());
 dbConnect();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/user',authRoter);
-app.use('/api/product',productRoter)
+app.use('/api/user', authRoter);
+app.use('/api/product', productRoter)
 app.use(notFound);
 app.use(errorHandler);
 app.listen(PORT, () => {
