2e70b5304d9a4f45b06d6084b3ff02c3a9f1019b
ThangNguyen-CT <thangnguyen1333@gmail.com>
2023-04-02 19:10:59 +0700

    deploy

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const router = express.Router();
 
-const 
-{
-    createProduct ,
+const {
+    createProduct,
     updateProduct,
     deleteProduct,
     getaProduct,
     getAllProduct,
-}= require('../controller/productCtrl');
+} = require('../controller/productCtrl');
 const { blogImgResize, uploadPhoto } = require("../middlewares/uploadImages");
 router.post("/",
-    uploadPhoto.array("images", 2),
+    uploadPhoto.array("images", 4),
     blogImgResize,
     createProduct);
-router.put("/:id",updateProduct);
-router.delete("/:id",deleteProduct),
-router.get('/:id',getaProduct),
-router.get('/',getAllProduct)
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct),
+    router.get('/:id', getaProduct),
+    router.get('/', getAllProduct)
 module.exports = router;
